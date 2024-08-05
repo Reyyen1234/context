febb60843ee77dfc282ef2b957f96bc1a9140859
Reyyen cuneyt <rrayyan213@gmail.com>
2024-08-05 17:16:28 +0300

    seventh commit

---
+++ src/components/Container.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react'
+import Header from './Header'
+import Button from './Button'
+import ThemeContext from './context/ThemeContext'
+function Container() {
+  const {theme}=useContext(ThemeContext)
+  return (
+    <div className={`app && ${theme}`}>
+      <Header />
+      <hr/>
+      <Button />
+    </div>
+  )
+}
+
+export default Container
